db5d89faf65e3933a209dd4aa7ac9ca5bb679dce
HinalSanandiya <91975051+HinalSanandiya@users.noreply.github.com>
2021-10-09 12:27:33 -0400

    Add files via upload

---
+++ P2/p2.js
@@ -0,0 +1,6 @@
+var os=require('os');
+console.log("Platform:"+ os.platform());
+console.log("Architecture:" + os.arch());
+console.log("User"+os.userInfo());
+console.log("Free Memory"+freemem());
+console.log("Total Memory"+totalmem());
